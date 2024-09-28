cad55c293f54c0d637101bb38dd1a51d9a56dbb3
cronobacter <antoniorafaelariasromero@gmail.com>
2024-09-28 12:15:28 -0500

    Add two.py and three.py

---
+++ sci/two.py
@@ -0,0 +1,58 @@
+import numpy as np
+import pandas as pd
+import matplotlib.pyplot as plt
+from sklearn.preprocessing import PolynomialFeatures
+from sklearn.linear_model import LinearRegression
+from sklearn.metrics import mean_squared_error
+
+poly = PolynomialFeatures(degree=2, include_bias=False)
+poly_reg_model = LinearRegression()
+
+x = [333.83,
+281.93,
+234.29,
+205.59,
+351.23,
+371.34]
+x = np.array(x)
+x.sort()
+
+y = [58,
+43,
+30,
+25,
+64,
+71]
+y.sort()
+
+# modelo de regresión polinómica
+poly_features = poly.fit_transform(x.reshape(-1,1))
+poly_reg_model.fit(poly_features, y)
+
+# predicción
+y_predicted = poly_reg_model.predict(poly_features)
+
+# obtener coeficientes
+coef = poly_reg_model.coef_
+intercept = poly_reg_model.intercept_
+
+# error medio cuadratico
+mse = mean_squared_error(y, y_predicted)
+
+plt.figure(figsize=(10,6))
+plt.xlabel("Tiempo (ms)")
+plt.ylabel("Altura (cm)")
+plt.title("Regresión Polinómica")
+plt.scatter(x,y, label="Datos originales")
+plt.plot(x, y_predicted, color="red", label="Regresion polinómica (grado 2)")
+plt.legend()
+plt.grid()
+props = dict(boxstyle='round', facecolor='lightblue', edgecolor='black', alpha=0.5)
+
+# coeficientes y mse
+plt.text(0.5, 0.1,
+         f'Ecuación: y = {coef[1]:.2}x² + {coef[0]:.2}x + {intercept:.2}\nMSE: {mse:.2}',
+         fontsize=12, transform=plt.gca().transAxes,
+         bbox=props)
+
+plt.show()
